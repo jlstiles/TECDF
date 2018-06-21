15104e0989edf7e9c967a6a6bfefee7a090a1142
Jonathan Levy <jl@jlstiles.com>
2018-06-20 19:48:08 -0700

    need to save the objects...

--- kernel_sim.R
+++ kernel_sim.R
@@ -157,7 +157,7 @@ for (n in c(100, 250, 500, 1000, 2500, 5000, 10000, 50000)) {
       geom_vline(xintercept=mean(res[,inds[1]]),color = colors[1])+
       geom_vline(xintercept=mean(res[,inds[2]]),color = colors[2])
     
-    assign(paste0("res", n, "unif", round(t[b],4)), list(plot = ggover, 
+    assign(paste0("res", n, "unif", b), list(plot = ggover, 
                                                    coverage = coverage, 
                                                    B = B, 
                                                    h = h_vec[bw], 
@@ -166,3 +166,17 @@ for (n in c(100, 250, 500, 1000, 2500, 5000, 10000, 50000)) {
 
   }
 }
+
+save(res100unif13, res100unif18, res100unif23, res100unif28, 
+     res250unif13, res250unif18, res250unif23, res250unif28,
+     res500unif13, res500unif18, res500unif23, res500unif28,
+     res1000unif13, res1000unif18, res1000unif23, res1000unif28,
+     res2500unif13, res2500unif18, res2500unif23, res2500unif28,
+     res5000unif13, res5000unif18, res5000unif23, res5000unif28,
+     res10000unif13, res10000unif18, res10000unif23, res10000unif28,
+     res50000unif13, res50000unif18, res50000unif23, res50000unif28,
+     res100unif_simul, res250unif_simul, res500unif_simul, res1000unif_simul,
+     res2500unif_simul, res5000unif_simul, res10000unif_simul, res50000unif_simul,
+     g0, Q0, file = "kernel_sim_unif.RData")
+
+100, 250, 500, 1000, 2500, 5000, 10000, 50000
