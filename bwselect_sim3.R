70f646a17560a1fd1dbdfba04ad9e665f1744155
Jonathan Levy <jl@jlstiles.com>
2018-09-15 16:59:23 -0700

    making more cores available

--- bwselect_sim3.R
+++ bwselect_sim3.R
@@ -53,7 +53,7 @@ for (j in 4) {
       truth = mean(true$blip> blip)
       B = 1000
       cl_size = ifelse(n > 10000, 12, 24)
-      cl = makeCluster(2, type = "SOCK")
+      cl = makeCluster(8, type = "SOCK")
       registerDoSNOW(cl)
       
       allresults=foreach(i=1:B,
