de1e49186efc97aa170a1210b3cb532061af96c9
Jonathan Levy <jl@jlstiles.com>
2018-09-15 17:20:55 -0700

    forgot tyhe sim fcns

---
+++ R/sim_fcns.R
@@ -0,0 +1,32 @@
+#' @export
+sim_bwselect = function(n, blip, bw_seq, g0, Q0, kernel)
+{
+  tmledata = gentmledata(n, d = 1, g0, Q0, V=1, formu = NULL)
+  r = length(bw_seq)
+  # undebug(gentmle_alt3)
+  select_info = lapply(bw_seq, FUN = function(h) {
+    info = gentmle_alt3(initdata=tmledata, estimate_fun = blipdist_estimate2,
+                 update_fun = blipdist_update, max_iter = 1000, kernel = kernel,
+                 simultaneous.inference = FALSE, blip = blip, h = h)
+    return(return(list(est = info$tmleests, IC = info$Dstar, SE = info$ED2)))
+    })
+  
+  ests = vapply(1:r, FUN = function(x) {
+    select_info[[x]]$est
+  }, FUN.VALUE = 1)
+  
+  SE = vapply(1:r, FUN = function(x) select_info[[x]]$SE/sqrt(n), FUN.VALUE = 1)
+  
+  IC = vapply(1:r, FUN = function(x) select_info[[x]]$IC, FUN.VALUE = rep(1,n))
+  
+  zscore = get.zscore(IC, .05)
+  
+  ci = cbind(ests = ests, left = ests - zscore*SE, right = ests + zscore*SE)
+  
+  return(list(ci = ci, zscore = zscore))
+  
+}
+  
+  
+
+  
