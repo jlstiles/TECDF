621dc55342f37478a92d53f3e6751877c343512f
Jonathan Levy <jl@jlstiles.com>
2018-09-16 15:09:36 -0700

    omp threads an issue apparently.  Clusters are fucked, true cluster fucks and dealing with a cluster is no

--- bwselect_sim1.sh
+++ bwselect_sim1.sh
@@ -7,6 +7,8 @@
 #$ -M jlstiles@berkeley.edu 
 #$ -m beas
 
+export OMP_NUM_THREADS=1
+
 R --vanilla < bwselect_sim1.R > bwselect_sim1.Rout
 
 
--- bwselect_sim2.sh
+++ bwselect_sim2.sh
@@ -7,6 +7,8 @@
 #$ -M jlstiles@berkeley.edu 
 #$ -m beas
 
+export OMP_NUM_THREADS=1
+
 R --vanilla < bwselect_sim2.R > bwselect_sim2.Rout
 
 
