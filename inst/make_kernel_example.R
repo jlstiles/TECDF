4000191510d627f9d7e728c701b4513d3f7aae0e
Jonathan Levy <jl@jlstiles.com>
2018-12-03 15:32:31 -0800

    final touch up

--- inst/make_kernel_example.R
+++ inst/make_kernel_example.R
@@ -2,7 +2,7 @@
 # smooth at -5 and 5. 
 order = 10
 R = 5
-k=make_kernel(order,R)
+k=blipCDF:::make_kernel(order,R)
 
 # check it is a kernel
 area = with(k, integrate(kern, lower = -R, upper = R, R = R, veck = veck, subdivisions = 10000)$value)
@@ -11,10 +11,11 @@ area
 # plot
 s = seq(-R,R,.001)
 y = with(k, kern(s, R=R, veck=veck))
-plot = qplot(x=s,y=y)+geom_line()
+plot = plot(s,y)
 plot
 
 # check orthogonality to a polynomial less than the order 
 test_fcn = function(x) (x^9+x^8)*with(k, kern(x, R=R, veck = veck))
 test_int = integrate(test_fcn, lower = -R, upper = R,subdivisions = 10000)
 test_int
+
